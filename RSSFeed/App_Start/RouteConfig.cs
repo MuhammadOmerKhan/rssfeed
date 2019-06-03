c20e5f2213c1902db6f149658645dc2738a30f80
Muhammad Omer Khan <muhammadomerkhan99@gmail.com>
2019-06-03 06:00:55 +0500

    code push

---
+++ RSSFeed/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RSSFeed
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "RSSFeed",
+                url: "rss",
+                defaults: new { controller = "Home", action = "RssFeed", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ RSSFeed/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+namespace RSSFeed.Controllers
+{
+    public class RSSActionResult : ActionResult
+    {
+        public SyndicationFeed Feed { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.ContentType = "application/rss+xml";
+
+            Rss20FeedFormatter rssFormatter = new Rss20FeedFormatter(Feed);
+            //This line enables Atom attribute in the header of RSS.
+            rssFormatter.SerializeExtensionsAsAtom = false;
+            using (XmlWriter writer = XmlWriter.Create(context.HttpContext.Response.Output))
+            {
+                rssFormatter.WriteTo(writer);
+            }
+        }
+    }
+    public class HomeController : Controller
+    {
+        public RSSActionResult RSSFeed()
+        {
+            //This line creates the title, description and link of your website.
+            SyndicationFeed feed = new SyndicationFeed("Muhammad Omer Khan", "This project allows you to create RSS Feed in ASP.NET MVC", new Uri("http://www.muhammadomerkhan.com"));
+
+            //This line helps you to create rss feed of your website.
+            List<SyndicationItem> items = new List<SyndicationItem>();
+            SyndicationItem item = new SyndicationItem("Test", "ABCD", new Uri("http://www.google.com"));
+            items.Add(item);
+            feed.Items = items;
+
+            return new RSSActionResult() { Feed = feed };
+        }
+    }
+}
